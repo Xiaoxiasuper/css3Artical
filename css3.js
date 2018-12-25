63da25cdbe2df506e53e37e5cf510d52a84d4af2
Xiaoxiasuper <932971296@qq.com>
2018-12-25 16:16:10 +0800

    finish

--- css3.js
+++ css3.js
@@ -1,21 +1,24 @@
 window.onload=function(){
-    var paraNum=500;
+    var paraNum=1200;
     
-    var hello=document.getElementsByClassName("hello")[0];
-    MyinnerHTML='<div class="canves1"><div class="chunk1"></div><div class="chunk"></div><div class="chunk1"></div><div class="chunk"></div><div class="chunk"></div><div class="chunk"></div><div class="chunk1"></div><div class="chunk"></div><div class="chunk1"></div></div>';
-
-    for(var i=0;i<paraNum;i++){
-        MyinnerHTML+=MyinnerHTML
+    var canves1 = $(".hello").find(".canves1");
+    for (var int = 0; int < paraNum; int++) {
+        canves1.clone().prependTo($(".hello"));
     }
+    var canves2 = $(".change").find(".canves2");
+    for (var int = 0; int <= paraNum; int++) {
+        canves2.clone().prependTo($(".change"));
+    }
+
     var canves=document.getElementsByClassName("canves1");
     var cols=0;
     var row=0;
-    for (var i = 0; i < canves.length; i++) {
+    for (var k = 0; k < paraNum; k++) {
         var myTop=-10*cols;
         var myLeft=10*row;
-        canves[i].style.top = myTop+'px';
-        canves[i].style.left = myLeft+'px';
-        if((i+1)%20==0){
+        canves[k].style.top = myTop+'px';
+        canves[k].style.left = myLeft+'px';
+        if((k+1)%40==0){
             row +=1;
             cols=0;
         }else{
@@ -26,12 +29,12 @@ window.onload=function(){
     var canves2=document.getElementsByClassName("canves2");
     var cols2=0;
     var row2=5;
-    for (var i = 0; i < canves.length; i++) {
+    for (var k = 0; k < paraNum+3; k++) {
         var myTop=-10*cols2;
         var myLeft=10*row2;
-        canves2[i].style.top = myTop+'px';
-        canves2[i].style.left = myLeft+'px';
-        if((i+1)%20==0){
+        canves2[k].style.top = myTop+'px';
+        canves2[k].style.left = myLeft+'px';
+        if((k+1)%40==0){
             row2 +=1;
             cols2=0;
         }else{
